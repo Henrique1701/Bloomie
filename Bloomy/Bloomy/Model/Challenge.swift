75ea33915ee03247edd1cd89e3ac0301e2997c92
v1lton <wrs@cin.ufpe.br>
2020-11-25 15:33:03 -0300

    Cria o arquivo Challenge.swift

---
+++ Bloomy/Bloomy/Model/Challenge.swift
@@ -0,0 +1,21 @@
+//
+//  Challenge.swift
+//  Bloomy
+//
+//  Created by Wilton Ramos on 25/11/20.
+//
+
+import CoreData
+import UIKit
+
+struct ChallengeManager {
+    // MARK: Create
+    
+    // MARK: Read
+    
+    // MARK: Update
+    
+    // MARK: Delete
+    
+    // MARK: Auxiliar
+}
