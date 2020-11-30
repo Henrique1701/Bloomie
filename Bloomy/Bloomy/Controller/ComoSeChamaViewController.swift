4a09e5fce1883a58c46dc5ef1df51d8a9277aac5
Alexandra <avz@cin.ufpe.br>
2020-11-30 19:40:20 -0300

    duas primeiras telas onboarding quase feitos

---
+++ Bloomy/Bloomy/Controller/ComoSeChamaViewController.swift
@@ -0,0 +1,29 @@
+//
+//  ComoSeChamaViewController.swift
+//  Bloomy
+//
+//  Created by Alexandra Zarzar on 30/11/20.
+//
+
+import UIKit
+
+class ComoSeChamaViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
