b5b9439c963b429aed0f5e5d98377a308a85e17a
v1lton <wrs@cin.ufpe.br>
2020-12-08 10:48:04 -0300

    Condiciona à tela inicial para onboarding ou home

--- Bloomy/Bloomy/Controller/AppDelegate.swift
+++ Bloomy/Bloomy/Controller/AppDelegate.swift
@@ -10,6 +10,7 @@ import CoreData
 
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
+    var window: UIWindow?
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
@@ -20,22 +21,36 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         // Change font of Tab Bar Items
         let fontAttributes = [NSAttributedString.Key.font: UIFont(name: "Poppins-Regular", size: 12.0)!]
         UITabBarItem.appearance().setTitleTextAttributes(fontAttributes, for: .normal)
+        
+        self.window = UIWindow(frame: UIScreen.main.bounds)
+        self.window?.makeKeyAndVisible()
+            
+        if (UserManager.shared.getUser() == nil) {
+            let storyboard = UIStoryboard(name: "Onboarding", bundle: nil)
+            let initialViewController = storyboard.instantiateInitialViewController()!
+            self.window?.rootViewController = initialViewController
+        } else {
+            let storyboard = UIStoryboard(name: "Home", bundle: nil)
+            let initialViewController = storyboard.instantiateInitialViewController()!
+            self.window?.rootViewController = initialViewController
+        }
+
         return true
     }
 
     // MARK: UISceneSession Lifecycle
 
-    func application(_ application: UIApplication, configurationForConnecting connectingSceneSession: UISceneSession, options: UIScene.ConnectionOptions) -> UISceneConfiguration {
-        // Called when a new scene session is being created.
-        // Use this method to select a configuration to create the new scene with.
-        return UISceneConfiguration(name: "Default Configuration", sessionRole: connectingSceneSession.role)
-    }
-
-    func application(_ application: UIApplication, didDiscardSceneSessions sceneSessions: Set<UISceneSession>) {
-        // Called when the user discards a scene session.
-        // If any sessions were discarded while the application was not running, this will be called shortly after application:didFinishLaunchingWithOptions.
-        // Use this method to release any resources that were specific to the discarded scenes, as they will not return.
-    }
+//    func application(_ application: UIApplication, configurationForConnecting connectingSceneSession: UISceneSession, options: UIScene.ConnectionOptions) -> UISceneConfiguration {
+//        // Called when a new scene session is being created.
+//        // Use this method to select a configuration to create the new scene with.
+//        return UISceneConfiguration(name: "Default Configuration", sessionRole: connectingSceneSession.role)
+//    }
+//
+//    func application(_ application: UIApplication, didDiscardSceneSessions sceneSessions: Set<UISceneSession>) {
+//        // Called when the user discards a scene session.
+//        // If any sessions were discarded while the application was not running, this will be called shortly after application:didFinishLaunchingWithOptions.
+//        // Use this method to release any resources that were specific to the discarded scenes, as they will not return.
+//    }
 
     // MARK: - Core Data stack
 
