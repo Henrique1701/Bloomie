26e35cc6fc0a16de31f115400ed0edf1bc9cacb4
Wilton Ramos <wrs@cin.ufpe.br>
2020-12-10 09:40:12 -0300

    Cria pasta para SKScenes

---
+++ Bloomy/Bloomy/Controller/SiriShortcutsViewController.swift
@@ -0,0 +1,29 @@
+//
+//  SiriShortcutsViewController.swift
+//  Bloomy
+//
+//  Created by Wilton Ramos on 10/12/20.
+//
+
+import UIKit
+
+class SiriShortcutsViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
