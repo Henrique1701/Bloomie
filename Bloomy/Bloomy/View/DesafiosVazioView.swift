3f10d2183ed0ee497ac3f9da01c15a3285d3b8a9
Wilton Ramos <wrs@cin.ufpe.br>
2020-12-10 11:44:27 -0300

    Cria ContentView.swift

---
+++ Bloomy/Bloomy/View/DesafiosVazioView.swift
@@ -0,0 +1,20 @@
+//
+//  DesafiosVazioView.swift
+//  Bloomy
+//
+//  Created by Wilton Ramos on 10/12/20.
+//
+
+import SwiftUI
+
+struct DesafiosVazioView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct DesafiosVazioView_Previews: PreviewProvider {
+    static var previews: some View {
+        DesafiosVazioView()
+    }
+}
