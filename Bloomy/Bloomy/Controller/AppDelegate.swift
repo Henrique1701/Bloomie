33632b119e01af2c85da58f62e06c5ec542e2590
Mayara Souza <mms8@cin.ufpe.br>
2021-01-08 15:56:45 -0300

    Muda a cor do Page Control

--- Bloomy/Bloomy/Controller/AppDelegate.swift
+++ Bloomy/Bloomy/Controller/AppDelegate.swift
@@ -11,9 +11,15 @@ import CoreData
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
     var window: UIWindow?
-
+    
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
+        
+        // Muda as cores das bolinhas do page control
+        let pageControl = UIPageControl.appearance()
+        // Color Literal
+        pageControl.currentPageIndicatorTintColor = #colorLiteral(red: 0.2431372549, green: 0.2470588235, blue: 0.2745098039, alpha: 1)
+        pageControl.pageIndicatorTintColor = #colorLiteral(red: 0.6000000238, green: 0.6000000238, blue: 0.6000000238, alpha: 1)
         
         // Change the color of tab bar items
         UITabBar.appearance().tintColor = #colorLiteral(red: 0.2431372549, green: 0.2470588235, blue: 0.2745098039, alpha: 1)
@@ -24,7 +30,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         
         self.window = UIWindow(frame: UIScreen.main.bounds)
         self.window?.makeKeyAndVisible()
-            
+        
         if (UserManager.shared.getUser() == nil) {
             let storyboard = UIStoryboard(name: "Onboarding", bundle: nil)
             let initialViewController = storyboard.instantiateInitialViewController()!
@@ -34,33 +40,33 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
             let initialViewController = storyboard.instantiateInitialViewController()!
             self.window?.rootViewController = initialViewController
         }
-
+        
         return true
     }
-
+    
     // MARK: UISceneSession Lifecycle
-
-//    func application(_ application: UIApplication, configurationForConnecting connectingSceneSession: UISceneSession, options: UIScene.ConnectionOptions) -> UISceneConfiguration {
-//        // Called when a new scene session is being created.
-//        // Use this method to select a configuration to create the new scene with.
-//        return UISceneConfiguration(name: "Default Configuration", sessionRole: connectingSceneSession.role)
-//    }
-//
-//    func application(_ application: UIApplication, didDiscardSceneSessions sceneSessions: Set<UISceneSession>) {
-//        // Called when the user discards a scene session.
-//        // If any sessions were discarded while the application was not running, this will be called shortly after application:didFinishLaunchingWithOptions.
-//        // Use this method to release any resources that were specific to the discarded scenes, as they will not return.
-//    }
-
+    
+    //    func application(_ application: UIApplication, configurationForConnecting connectingSceneSession: UISceneSession, options: UIScene.ConnectionOptions) -> UISceneConfiguration {
+    //        // Called when a new scene session is being created.
+    //        // Use this method to select a configuration to create the new scene with.
+    //        return UISceneConfiguration(name: "Default Configuration", sessionRole: connectingSceneSession.role)
+    //    }
+    //
+    //    func application(_ application: UIApplication, didDiscardSceneSessions sceneSessions: Set<UISceneSession>) {
+    //        // Called when the user discards a scene session.
+    //        // If any sessions were discarded while the application was not running, this will be called shortly after application:didFinishLaunchingWithOptions.
+    //        // Use this method to release any resources that were specific to the discarded scenes, as they will not return.
+    //    }
+    
     // MARK: - Core Data stack
-
+    
     lazy var persistentContainer: NSPersistentContainer = {
         /*
          The persistent container for the application. This implementation
          creates and returns a container, having loaded the store for the
          application to it. This property is optional since there are legitimate
          error conditions that could cause the creation of the store to fail.
-        */
+         */
         let container = NSPersistentContainer(name: "Bloomy")
         container.loadPersistentStores(completionHandler: { (_, error) in
             if let error = error as NSError? {
@@ -79,9 +85,9 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         })
         return container
     }()
-
+    
     // MARK: - Core Data Saving support
-
+    
     func saveContext () {
         let context = persistentContainer.viewContext
         if context.hasChanges {
@@ -95,5 +101,5 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
             }
         }
     }
-
+    
 }
