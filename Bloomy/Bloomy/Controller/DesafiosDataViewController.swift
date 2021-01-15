7fea98aaf81dc049cdead24222dd88ec5844b8c7
Wilton Ramos <wrs@cin.ufpe.br>
2021-01-15 15:12:37 -0300

    Faz com que os botões de concluir estejam conectados às ilhas

--- Bloomy/Bloomy/Controller/DesafiosDataViewController.swift
+++ Bloomy/Bloomy/Controller/DesafiosDataViewController.swift
@@ -11,15 +11,13 @@ class DesafiosDataViewController: UIViewController {
     
     // Esse View Controller define como serão as páginas do PageViewController
     
+    // MARK: Variáveis Globais
+    var islandName: String?
+    
     // Outlets
     @IBOutlet weak var card: UIImageView!
     @IBOutlet weak var summaryLabel: UILabel!
     @IBOutlet weak var concluirButtonOutlet: UIButton!
-    
-    // Botão concluir missão
-    @IBAction func concluirButton(_ sender: Any) {
-        
-    }
     
     // Global variables
     var cardImage: UIImage!
@@ -34,4 +32,29 @@ class DesafiosDataViewController: UIViewController {
         card.clipsToBounds = true
         card.layer.cornerRadius = 20
     }
+    
+    // Botão concluir missão
+    @IBAction func concluirButton(_ sender: Any) {
+        print("meu nome é \(islandName ?? "Sem nome")")
+    }
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        let destination = segue.destination as? IslandsViewController
+        destination?.senderWasDesafios = true
+        
+        if islandName == IslandsNames.health.rawValue {
+            destination?.island = IslandManager.shared.getIsland(withName: IslandsNames.health.rawValue)!
+            destination?.sceneName = "HealthIsland"
+        } else if islandName == IslandsNames.leisure.rawValue {
+            destination?.island = IslandManager.shared.getIsland(withName: IslandsNames.leisure.rawValue)!
+            destination?.sceneName = "LeisureIsland"
+        } else if islandName == IslandsNames.mindfulness.rawValue {
+            destination?.island = IslandManager.shared.getIsland(withName: IslandsNames.mindfulness.rawValue)!
+            destination?.sceneName = "MindfulnessIsland"
+        } else if islandName == IslandsNames.loveds.rawValue {
+            destination?.island = IslandManager.shared.getIsland(withName: IslandsNames.loveds.rawValue)!
+            destination?.sceneName = "LovedsIsland"
+        }
+    }
+    
 }
