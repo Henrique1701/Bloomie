e68b41ba9d5feaa93649330ad14b43e3a05c7ed0
Wilton Ramos <wrs@cin.ufpe.br>
2021-02-26 16:49:20 -0300

    Adiciona Onboarding.Swift

---
+++ Bloomy/Bloomy/View/Onboarding.swift
@@ -0,0 +1,20 @@
+//
+//  Onboarding.swift
+//  Bloomy
+//
+//  Created by Wilton Ramos on 26/02/21.
+//
+
+import SwiftUI
+
+struct Onboarding: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct Onboarding_Previews: PreviewProvider {
+    static var previews: some View {
+        Onboarding()
+    }
+}
