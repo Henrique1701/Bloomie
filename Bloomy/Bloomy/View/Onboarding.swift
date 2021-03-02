a92f5fde00730c72f05f9dec546c001d4557c78a
Wilton Ramos <wrs@cin.ufpe.br>
2021-03-02 16:28:52 -0300

    Constroi front da primeira tela de onboarding

--- Bloomy/Bloomy/View/Onboarding.swift
+++ Bloomy/Bloomy/View/Onboarding.swift
@@ -4,45 +4,47 @@
 //
 //  Created by Wilton Ramos on 26/02/21.
 //
-
-/*TODO:
- [] Colocar um tamanho máximo para a imagem para
- [] FontSize ter manho mínimo e máximo
- [] Ver SwiftUI em proporção à tela
- [] Ver GeomtryReader
- [] Ver iPad nas Highs
- */
-
 import SwiftUI
 
 struct Onboarding: View {
     var body: some View {
-        VStack {
-            Image("garota_onboarding")
-                .resizable()
-                .scaledToFit()
-                .frame(maxWidth: 600, maxHeight: 600)
-            
-            Spacer()
-            
-            Text("Como vai?")
-                .font(.custom("Poppins-SemiBold", size: 30))
-            
-            Text("Acreditamos que pequenas ações\n conseguem mudar o mundo")
-                .font(.custom("Poppins-Regular", size: 16))
-            
-            Spacer()
-            
-            Button(action: /*@START_MENU_TOKEN@*//*@PLACEHOLDER=Action@*/{}/*@END_MENU_TOKEN@*/) {
-                Text("Começar")
-                    .font(.custom("Poppins-Bold", size: 18))
-                    .foregroundColor(.black)
-                    .padding(.vertical, 10)
-                    .padding(.horizontal, 30)
+        
+        GeometryReader { geometry in
+            VStack() {
+                
+                Image("garota_onboarding")
+                    .resizable()
+                    .scaledToFit()
+                    .frame(idealWidth: 409, maxWidth: 600,idealHeight: 402, maxHeight: geometry.size.height * 0.5)
+                
+                Text("Como vai?")
+                    .font(.custom("Poppins-SemiBold", size: 34))
+                    .foregroundColor(Color("cor_fonte"))
+                    
+                Spacer()
+                    .frame(maxHeight: 20)
+                
+                Text("Acreditamos que pequenas ações\n conseguem mudar o mundo")
+                    .font(.custom("Poppins-Regular", size: 18))
+                    .minimumScaleFactor(0.5)
+                    .multilineTextAlignment(.center)
+                    .foregroundColor(Color("cor_fonte"))
+                
+                Spacer(minLength: 40)
+                
+                Button(action: /*@START_MENU_TOKEN@*//*@PLACEHOLDER=Action@*/{}/*@END_MENU_TOKEN@*/) {
+                    Text("começar")
+                        .font(.custom("Poppins-Bold", size: 18))
+                        .foregroundColor(.black)
+                        .padding(.vertical, geometry.size.height * 0.02)
+                        .padding(.horizontal, geometry.size.width * 0.15)
+                }
+                .background(Color("cor_botao"))
+                .clipShape(Capsule())
+                
             }
-            .background(Color("cor_botao"))
-            .clipShape(Capsule())
-        }
+            .padding(.bottom, geometry.size.height * 0.05)
+        }.background(Color("cor_fundo").edgesIgnoringSafeArea(.all))
     }
 }
 
@@ -50,8 +52,6 @@ struct Onboarding_Previews: PreviewProvider {
     static var previews: some View {
         Group {
             Onboarding()
-            Onboarding()
-                .previewDevice("iPad Pro (12.9-inch) (4th generation)")
             Onboarding()
                 .previewDevice("iPod touch (7th generation)")
         }
