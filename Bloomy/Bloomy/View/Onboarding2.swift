b18dbb3a8951abbfa645d9c96cb69f984b16225d
Wilton Ramos <wrs@cin.ufpe.br>
2021-03-02 18:25:30 -0300

    Inicia a construção do front da segunda tela de onboarding

--- Bloomy/Bloomy/View/Onboarding2.swift
+++ Bloomy/Bloomy/View/Onboarding2.swift
@@ -9,12 +9,43 @@ import SwiftUI
 
 struct Onboarding2: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        
+        GeometryReader { geometry in
+            
+            VStack() {
+                Text("Como você se chama?")
+                    .font(.custom("Poppins-Bold", size: 28))
+                    .padding(.trailing)
+                    .frame(width: geometry.size.width * 0.9, alignment: .leading)
+                    
+                
+                TextField(/*@START_MENU_TOKEN@*/"Placeholder"/*@END_MENU_TOKEN@*/, text: /*@START_MENU_TOKEN@*//*@PLACEHOLDER=Value@*/.constant("")/*@END_MENU_TOKEN@*/)
+                    .frame(width: geometry.size.width * 0.9)
+                
+                
+                Spacer ()
+                
+                Button(action: /*@START_MENU_TOKEN@*//*@PLACEHOLDER=Action@*/{}/*@END_MENU_TOKEN@*/) {
+                    Text("continuar")
+                        .font(.custom("Poppins-Bold", size: 18))
+                        .foregroundColor(.black)
+                        .padding(.vertical, geometry.size.height * 0.02)
+                        .padding(.horizontal, geometry.size.width * 0.15)
+                }
+                .background(Color("cor_botao"))
+                .clipShape(Capsule())
+            }
+            .padding(.bottom, geometry.size.height * 0.05)
+            .frame(width: geometry.size.width)
+            
+        }.background(Color("cor_fundo").edgesIgnoringSafeArea(.all))
     }
 }
 
 struct Onboarding2_Previews: PreviewProvider {
     static var previews: some View {
         Onboarding2()
+        Onboarding2()
+            .previewDevice("iPod touch (7th generation)")
     }
 }
