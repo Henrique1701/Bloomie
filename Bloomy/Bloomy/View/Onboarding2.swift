674eca96aaf8b2d4c0bba16a873ecfa6ab2a4e38
Wilton Ramos <wrs@cin.ufpe.br>
2021-03-03 17:49:19 -0300

    Constroi navegação da segunda tela para terceira

--- Bloomy/Bloomy/View/Onboarding2.swift
+++ Bloomy/Bloomy/View/Onboarding2.swift
@@ -8,11 +8,22 @@
 import SwiftUI
 
 struct Onboarding2: View {
+    
+    @Environment(\.presentationMode) var presentationMode: Binding<PresentationMode>
+    @State var textFieldInput = ""
+    
     var body: some View {
         
         GeometryReader { geometry in
             
             VStack() {
+                
+                Button(action: {self.presentationMode.wrappedValue.dismiss()}) {
+                    Image("botao_back")
+                        .aspectRatio(contentMode: .fit)
+                        .foregroundColor(.white)
+                }.frame(width: geometry.size.width * 0.9, alignment: .leading)
+                
                 Text("Como posso te chamar?")
                     .font(.custom("Poppins-Medium", size: 30))
                     .foregroundColor(Color("cor_fonte"))
@@ -20,35 +31,54 @@ struct Onboarding2: View {
                     .frame(width: geometry.size.width * 0.9, alignment: .leading)
                 
                 
-                TextField("Aninha", text: .constant(""))
+                TextField("Aninha", text: $textFieldInput)
                     .frame(width: geometry.size.width * 0.9, alignment: .leading)
                 
                 Rectangle()
                     .background(Color("cor_fonte"))
-                    .opacity(0.6)
-                    .position(x: geometry.size.width * 0.30)
-                    .frame(width: geometry.size.width * 0.75, height: 3, alignment: .leading)
-                
+                    .opacity(0.4)
+                    //.position(x: geometry.size.width * 0.30)
+                    .frame(width: geometry.size.width * 0.9, height: 3, alignment: .leading) //0.75
+                    
                 
                 
                 Spacer ()
                 
-                Button(action: {}) {
-                    Text("continuar")
-                        .font(.custom("Poppins-Bold", size: 18))
-                        .foregroundColor(Color("cor_fonte"))
-                        .padding(.vertical, geometry.size.height * 0.02)
-                        .padding(.horizontal, geometry.size.width * 0.15)
+                if textFieldInput == "" {
+                    Button(action: /*@START_MENU_TOKEN@*//*@PLACEHOLDER=Action@*/{}/*@END_MENU_TOKEN@*/) {
+                        Text("continuar")
+                            .font(.custom("Poppins-Bold", size: 18))
+                            .foregroundColor(Color("cor_fonte"))
+                            .padding(.vertical, geometry.size.height * 0.02)
+                            .padding(.horizontal, geometry.size.width * 0.15)
+                    }
+                    .background(Color("cor_botao"))
+                    .opacity(0.2)
+                    .clipShape(Capsule())
+                    .disabled(true)
+                } else {
+                    NavigationLink(
+                        destination: Onboarding3(),
+                        label: {
+                            Text("continuar")
+                                .font(.custom("Poppins-Bold", size: 18))
+                                .foregroundColor(Color("cor_fonte"))
+                                .padding(.vertical, geometry.size.height * 0.02)
+                                .padding(.horizontal, geometry.size.width * 0.15)
+                        })
+                        .background(Color("cor_botao"))
+                        .clipShape(Capsule())
                 }
-                .background(Color("cor_botao"))
-                .clipShape(Capsule())
+                
             }
             .padding(.bottom, geometry.size.height * 0.05)
             .frame(width: geometry.size.width)
             
         }.background(Color("cor_fundo").edgesIgnoringSafeArea(.all))
+        .navigationBarHidden(true)
     }
 }
+
 
 struct Onboarding2_Previews: PreviewProvider {
     static var previews: some View {
