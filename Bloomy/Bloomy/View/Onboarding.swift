78a2c14eb60b8d49d52e44391ce22c99abccddf4
Wilton Ramos <wrs@cin.ufpe.br>
2021-03-03 17:48:43 -0300

    Constroi navegação da primeira tela para segunda

--- Bloomy/Bloomy/View/Onboarding.swift
+++ Bloomy/Bloomy/View/Onboarding.swift
@@ -9,44 +9,51 @@ import SwiftUI
 struct Onboarding: View {
     var body: some View {
         
-        GeometryReader { geometry in
-            VStack() {
-                
-                Image("garota_onboarding")
-                    .resizable()
-                    .scaledToFit()
-                    .frame(idealWidth: 409, maxWidth: 600,idealHeight: 402, maxHeight: geometry.size.height * 0.5)
-                
-                Text("Como vai?")
-                    .font(.custom("Poppins-SemiBold", size: 34))
-                    .foregroundColor(Color("cor_fonte"))
+        NavigationView {
+            
+            GeometryReader { geometry in
+                VStack() {
                     
-                Spacer()
-                    .frame(maxHeight: 20)
-                
-                Text("Acreditamos que pequenas ações\n conseguem mudar o mundo")
-                    .frame(width: geometry.size.width * 0.9)
-                    .font(.custom("Poppins-Regular", size: 18))
-                    .lineLimit(2)
-                    .minimumScaleFactor(0.5)
-                    .multilineTextAlignment(.center)
-                    .foregroundColor(Color("cor_fonte"))
-                
-                Spacer(minLength: 40)
-                
-                Button(action: /*@START_MENU_TOKEN@*//*@PLACEHOLDER=Action@*/{}/*@END_MENU_TOKEN@*/) {
-                    Text("começar")
-                        .font(.custom("Poppins-Bold", size: 18))
+                    Image("garota_onboarding")
+                        .resizable()
+                        .scaledToFit()
+                        .frame(idealWidth: 409, maxWidth: 600,idealHeight: 402, maxHeight: geometry.size.height * 0.5)
+                    
+                    Text("Como vai?")
+                        .font(.custom("Poppins-SemiBold", size: 34))
                         .foregroundColor(Color("cor_fonte"))
-                        .padding(.vertical, geometry.size.height * 0.02)
-                        .padding(.horizontal, geometry.size.width * 0.15)
+                        
+                    Spacer()
+                        .frame(maxHeight: 20)
+                    
+                    Text("Acreditamos que pequenas ações\n conseguem mudar o mundo")
+                        .frame(width: geometry.size.width * 0.9)
+                        .font(.custom("Poppins-Regular", size: 18))
+                        .lineLimit(2)
+                        .minimumScaleFactor(0.5)
+                        .multilineTextAlignment(.center)
+                        .foregroundColor(Color("cor_fonte"))
+                    
+                    Spacer(minLength: 40)
+                    
+                    NavigationLink(
+                        destination: Onboarding2(),
+                        label: {
+                            Text("começar")
+                                .font(.custom("Poppins-Bold", size: 18))
+                                .foregroundColor(Color("cor_fonte"))
+                                .padding(.vertical, geometry.size.height * 0.02)
+                                .padding(.horizontal, geometry.size.width * 0.15)
+                        })
+                        .background(Color("cor_botao"))
+                        .clipShape(Capsule())
+
                 }
-                .background(Color("cor_botao"))
-                .clipShape(Capsule())
-                
-            }
-            .padding(.bottom, geometry.size.height * 0.05)
-        }.background(Color("cor_fundo").edgesIgnoringSafeArea(.all))
+                .padding(.bottom, geometry.size.height * 0.05)
+            }.background(Color("cor_fundo").edgesIgnoringSafeArea(.all))
+            .navigationBarHidden(true)
+        }
+        
     }
 }
 
