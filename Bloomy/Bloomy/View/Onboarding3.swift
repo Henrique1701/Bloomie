60af7e1c0c9490e3ed233d2e0c21f662bce1333e
Wilton Ramos <wrs@cin.ufpe.br>
2021-03-03 15:55:54 -0300

    Adiciona onboarding3 ao projeto

---
+++ Bloomy/Bloomy/View/Onboarding3.swift
@@ -0,0 +1,20 @@
+//
+//  Onboarding3.swift
+//  Bloomy
+//
+//  Created by Wilton Ramos on 03/03/21.
+//
+
+import SwiftUI
+
+struct Onboarding3: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct Onboarding3_Previews: PreviewProvider {
+    static var previews: some View {
+        Onboarding3()
+    }
+}
