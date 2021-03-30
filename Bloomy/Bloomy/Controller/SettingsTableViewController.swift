13e170f9d3e7c750181bef52e6e23890842c2b78
José Henrique Fernandes Silva <jhfs@cin.ufpe.br>
2021-03-30 15:36:47 -0300

    Substitui string por UserDefaultsKeys no arquivo SettingsTableViewController.swift

--- Bloomy/Bloomy/Controller/SettingsTableViewController.swift
+++ Bloomy/Bloomy/Controller/SettingsTableViewController.swift
@@ -21,14 +21,14 @@ public class SettingsTableViewController: UITableViewController {
         // self.navigationItem.rightBarButtonItem = self.editButtonItem
         
         // Altera o estado do switch
-        let stateSoundsSwitch = UserDefaults.standard.bool(forKey: "stateSoundsSwitch")
+        let stateSoundsSwitch = UserDefaults.standard.bool(forKey: UserDefaultsKeys.stateSoundsSwitch)
         self.soundsSwitch.setOn(stateSoundsSwitch, animated: true)
 
     }
 
     @IBAction func touchedSoundsSwitch(_ sender: Any) {
         let stateSoundsSwitch = self.soundsSwitch.isOn
-        UserDefaults.standard.set(stateSoundsSwitch, forKey: "stateSoundsSwitch")
+        UserDefaults.standard.set(stateSoundsSwitch, forKey: UserDefaultsKeys.stateSoundsSwitch)
         MusicPlayer.shared.changeStatePlayer(state: stateSoundsSwitch)
     }
     // MARK: - Table view data source
