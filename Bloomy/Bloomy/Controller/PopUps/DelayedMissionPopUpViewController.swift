7c5ad070ce9266ad4dceb0c45e44d667ac85a649
Wilton Ramos <wiltonramos@Maquinho-de-Wilton.local>
2021-04-16 15:32:17 -0300

    Modifica label com a missão em aberto

--- Bloomy/Bloomy/Controller/PopUps/DelayedMissionPopUpViewController.swift
+++ Bloomy/Bloomy/Controller/PopUps/DelayedMissionPopUpViewController.swift
@@ -8,21 +8,33 @@
 import UIKit
 
 class DelayedMissionPopUpViewController: UIViewController {
+    @IBOutlet weak var missionLabel: UILabel!
     @IBOutlet weak var doneMissionButton: UIButton!
     @IBOutlet weak var dismissMissionButton: UIButton!
     
     var challenge: Challenge?
+    let dailyMissionManager = DailyMissionsManager.shared
+    let haptics = UINotificationFeedbackGenerator()
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        self.setupMissionLabelStyle()
     }
+    
+    // MARK: - Style
+    
+    private func setupMissionLabelStyle() {
+        self.missionLabel.text = challenge?.summary
+    }
+    
+    // MARK: - Actions
     @IBAction func doneMission(_ sender: Any) {
+        dailyMissionManager.setDoneStatus(forChallenge: self.challenge)
+        self.dismiss(animated: true, completion: nil)
+        haptics.notificationOccurred(.success)
     }
     
     @IBAction func dismissMission(_ sender: Any) {
         self.dismiss(animated: true, completion: nil)
     }
-    
 }
